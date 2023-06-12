79b20781de636ccd9143bc26fa14f5fc91b68759
thalisson-sousa <thalissonpop91@gmail.com>
2023-06-11 22:38:55 -0300

    feat: add initial html structure and header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import "./header.scss"
+
+export default function header() {
+    return (
+        <div className="header">
+        <div>
+          <h1>Hi, i´m Thalisson 👋</h1>
+          <h2>FrontEnd Developer</h2>
+        </div>
+        <Image
+          src="/perfil.png"
+          alt="Next.js Logo"
+          width={325}
+          height={270}
+          priority
+        />
+      </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
-import Image from 'next/image'
+import Header from "./components/header/header";
+
+import "./styles/home.scss";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <Image
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    <main className="container">
+      <Header />
+      <div className="experience">
+        <h3>Experience</h3>
+        <p>
+          3 years working as a Software Developer, in companys such as Itaú,
+          Stone and Aftersale.
+        </p>
+        <div className="experience-time"></div>
+
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+            <span>✈️ EN - Initial</span>
+            <span>✈️ PT-BR - Native Speaker</span>
+          </div>
+          <div className="educational-info">
+            <h3>Education</h3>
+            <span>
+            👨‍🎓 Computer Science Bachelors Degree - Universidade Federal de
+              Pelotas
+            </span>
+          </div>
+        </div>
+        <div className="buttons">
+          <div className="social"></div>
+          <button>Contact me</button>
+        </div>
       </div>
     </main>
-  )
+  );
 }
